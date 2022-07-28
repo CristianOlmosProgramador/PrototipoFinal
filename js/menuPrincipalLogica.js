20e76222ac13cf986bceb391cb22476b81fdc22f
CristianOlmos-ProgramadorSoftware <cr.olmos25@gmail.com>
2022-07-27 22:11:12 -0400

    subir cambios

--- js/menuPrincipalLogica.js
+++ js/menuPrincipalLogica.js
@@ -1,8 +1,29 @@
-'use strict';
 
-function  mensajeExitoso () {
-    swal("Bienvenido", "Inicio de session correcto", "success");
-    return true;
-}
+const BtnCerrarSession = document.querySelector("#cerrar");
 
-mensajeExitoso();
+
+BtnCerrarSession.addEventListener('click',()=>{
+ 
+    Swal.fire({
+        title: '¿Está seguro que desea cerrar su sessión?',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'SI',
+        denyButtonText: 'NO',
+        customClass: {
+          actions: 'my-actions',
+          cancelButton: 'order-1 right-gap',
+          confirmButton: 'order-2',
+          denyButton: 'order-3',
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+         // Swal.fire('Saved!', '', 'success')
+         window.open('index.html');
+        } else if (result.isDenied) {
+        //   Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+});
+
+
