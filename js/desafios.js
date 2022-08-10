681f2a06cf99f1da9739b2ca38d0f10f58c665ee
CristianOlmos-ProgramadorSoftware <cr.olmos25@gmail.com>
2022-08-10 13:23:33 -0400

    Subir desafios

--- js/desafios.js
+++ js/desafios.js
@@ -1,2 +1,57 @@
 // Solo para desafios desde la rama Desafios. 
 
+//Capturar campos del formulario 
+
+const btnAgregar = document.querySelector('#btnAgregar'); 
+const nombreMascota = document.querySelector('#nombre'); 
+const tipoMascota = document.querySelector('#tipoMascota');
+
+
+const btnVer = document.querySelector('#mostrarArray'); 
+
+
+// array para guardar mascota. 
+const arrayMascota = [];
+
+
+// evento click del boton
+btnAgregar.addEventListener("click", ()=>{
+    
+    if(!nombreMascota.value == "" && !tipoMascota.value == ""){
+        console.log('su mascota se agrego correctamente');
+        let objetoMascota = {nombre:     nombreMascota.value, 
+                            tipoMascota : tipoMascota.value}
+                     
+        arrayMascota.push(objetoMascota)
+        limpiarCajas();
+        cambiaVerde();
+
+    }else{
+        cambiaRojo();
+        console.log('por favor ingresar mascota');
+    }
+
+
+});
+
+
+btnVer.addEventListener('click',( )=>{     
+console.log(arrayMascota);
+
+});
+
+
+function cambiaRojo() {
+    btnAgregar.style.backgroundColor= "red";
+  }
+
+  function cambiaVerde() {
+    btnAgregar.style.backgroundColor = "green";
+  }
+
+  const limpiarCajas = ()=>{
+    nombreMascota.value = ''; 
+    tipoMascota.value = '';
+  }
+
+console.log(arrayMascota);
